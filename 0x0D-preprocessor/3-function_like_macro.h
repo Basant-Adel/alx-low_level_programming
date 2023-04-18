770f5f38c55f3ff4a489a1b94b0c65122017ade8
Basant-Adel <basantadel99@gmail.com>
2023-04-17 17:14:41 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTION_LINK_MACRO_H
+#define _FUNCTION_LINK_MACRO_H
+/* A function-like macro ABS(b) */
+#define ABS(b) ((b) < 0 ? (-(b)) : (b))
+/* That computes the absolute value of a number b */
+#endif

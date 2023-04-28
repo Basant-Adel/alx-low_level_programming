ce00b20f3e328bbc946f22750c5bd4347908f8fa
Basant-Adel <basantadel99@gmail.com>
2023-04-27 17:30:18 -0700

    5. The Hare and the Tortoise (100-first.c)

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor - A function that prints You're beat!
+ *and yet, you must allow,\nI bore my house upon my back!\n
+ *before the main function is executed.
+ *
+ *Return: void
+ */
+
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}

2217aa0f4a985b11931ecc0c8ccf017c8bf90536
Basant-Adel <basantadel99@gmail.com>
2023-05-02 16:32:37 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,4 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
+
+/**
+ * _ra - It's reallocates memory for array of pointers to node in linked list
+ *
+ *@list: The old list
+ *@size: It's a size of the new list
+ *@nl: It's a new node from list
+ *Return: The new list
+ */
+
+listint_t **_ra(listint_t **list, size_t size, listint_t *nl)
+{
+	listint_t **newlist;
+	size_t b;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (b = 0; b < size - 1; b++)
+		newlist[b] = list[b];
+	newlist[b] = nl;
+	free(list);
+	return (newlist);
+}
 
 /**
  * free_listint_safe - A function that frees a listint_t list
@@ -10,32 +39,29 @@
 
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *currentnode = *h, *node;
-	size_t counter = 0;
+	size_t b, n_node = 0;
+	listint_t **list = NULL;
+	listint_t *next;
 
-	if (!h || !*h)
+	if (head == NULL || *head == NULL)
+		return (n_node);
+	while (*head != NULL)
 	{
-		return (counter);
-	}
-
-	while (currentnode)
-	{
-		node = currentnode;
-		currentnode = currentnode->next;
-
-		if (currentnode <= currentnode->next)
+		for (b = 0; b < n_node; b++)
 		{
-			*h = NULL;
-			counter++;
-			break;
+			if (*head == list[b])
+			{
+				*head = NULL;
+				free(list);
+				return (n_node);
+			}
 		}
-
-		node = currentnode->next;
-		currentnode->next = NULL;
-		free(currentnode);
-		currentnode = node;
-		counter++;
+		n_node++;
+		list = _ra(list, n_node, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 	}
-	*h = NULL;
-	return (counter);
+	free(list);
+	return (n_node);
 }

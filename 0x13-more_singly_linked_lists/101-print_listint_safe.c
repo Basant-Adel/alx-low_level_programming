818b0df24388cde6f7dd718ac2a40f31762804a1
Basant-Adel <basantadel99@gmail.com>
2023-05-02 15:19:40 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
+
+/**
+ * _r - It's reallocates memory for array of pointers to node in linked list
+ *
+ *@list: The old list
+ *@size: It's a size of the new list
+ *@nl: It's a new node from list
+ *Return: The new list
+ */
+
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *nl)
+{
+	const listint_t **new_l;
+	size_t b;
+
+	new_l = malloc(size * sizeof(listint_t *));
+
+	if (new_l == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+
+	for (b = 0; b < size - 1; b++)
+		new_l[b] = list[b];
+	new_l[b] = nl;
+	free(list);
+	return (new_l);
+}
 
 /**
  * print_listint_safe - A function that prints a listint_t linked list
@@ -8,30 +39,26 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *newnode, *b;
-	size_t counter = 0;
+	size_t b, newnode = 0;
+	const listint_t **list = NULL;
 
-	if (!head || !head->next)
+	while (head != NULL)
 	{
-		return (0);
-	}
-
-	newnode = head;
-	while (newnode)
-	{
-		printf("[%p] %d\n", (void *)newnode, newnode->n);
-		counter++;
-
-		if (newnode > newnode->next)
+		for (b = 0; b < newnode; b++)
 		{
-			newnode = newnode->next;
+
+			if (head == list[b])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(list);
+				return (newnode);
+			}
 		}
-		else
-		{
-			b = newnode->next;
-			printf("-> [%p] %d\n", (void *)b, b->n);
-			break;
-		}
+		newnode++;
+		list = _r(list, newnode, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
-	return (counter);
+	free(list);
+	return (newnode);
 }

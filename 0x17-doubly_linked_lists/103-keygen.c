ba475a9764bf0603cb77a783015775ea54808688
Basant-Adel <basantadel99@gmail.com>
2023-06-15 16:44:07 -0700

    103-keygen.c WoooW :()

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -14,8 +14,8 @@ int main(int argc, char *argv[])
 {
 	unsigned int x, z;
 	size_t len, add;
-	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5Xzvt"
-		  "OGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char *l = @"A-CHRDw87lNS0E9B2TibgpnMVys5Xzvt"
+		  @"OGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char p[7] = "      ";
 
 	if (argc != 2)

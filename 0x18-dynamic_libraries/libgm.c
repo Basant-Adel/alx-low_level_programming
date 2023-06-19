e0b57f5f954b05e4d3f684f8cd3018587ff42b13
Basant-Adel <basantadel99@gmail.com>
2023-06-19 23:39:11 +0000

    3

---
+++ 0x18-dynamic_libraries/libgm.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * printf - A function to Print the "Please make me win!" message
+ *@format: It's a String Format
+ *Return: 0 (EXIT_SUCCESS)
+*/
+
+int printf(const char *format, ...)
+{
+	write(1, "9 8 10 24 75 - 9\n", 18);
+	write(1, "Congratulations, you win the Jackpot!\n", 39);
+	exit(EXIT_SUCCESS);
+}

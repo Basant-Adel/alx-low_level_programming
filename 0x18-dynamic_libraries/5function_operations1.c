70a8a74c329563b9e9b1e9bc666ea4606ffe36bd
Basant-Adel <basantadel99@gmail.com>
2023-06-19 14:57:05 -0700

    100-operations.so

---
+++ 0x18-dynamic_libraries/5function_operations1.c
@@ -0,0 +1,61 @@
+#include "sum.h"
+
+/**
+ * add - A function to Adds two integers
+ *@a: It's a First integer
+ *@b: It's a Second integer
+ *Return: Sum of a and b
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - A function to Subtracts two integers
+ *@a: It's a First integer
+ *@b: It's a Second integer
+ *Return: Difference of a and b
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - A function to Multiplies two integers
+ *@a: It's a First integer
+ *@b: It's a Second integer
+ *Return: Product of a and b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - A function to Divides two integers
+ *@a: It's a First integer
+ *@b: It's a Second integer
+ *Return: Quotient of a and b
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - A function to Modulus of two integers
+ *@a: It's a First integer
+ *@b: It's a Second integer
+ *Return: Remainder of a and b
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}

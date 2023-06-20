9374f87e262e69d4f9137acfc06a5a5642945934
Basant-Adel <basantadel99@gmail.com>
2023-06-20 01:30:13 +0000

    try ohhhhhhhhhhhhhh :(

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Basant-Adel/alx-low_level_programming/raw/6f936b80b0d9fd8edcfaf23efe7b2d0f684c4183/0x18-dynamic_libraries/librand_override.so
-export LD_PRELOAD=/tmp/librand_override.so
+wget -P /tmp https://github.com/Basant-Adel/alx-low_level_programming/raw/6f936b80b0d9fd8edcfaf23efe7b2d0f684c4183/0x18-dynamic_libraries/libgm.so
+export LD_PRELOAD=/tmp/libgm.so
